71dafe25a59b8b0fb4179c83a1b9c23398a49c64
Vitorhugofsousa <vitorhugofs2018@gmail.com>
2025-02-09 12:33:03 -0300

    adicionando comentarios ao codigo

--- Serial_Com.c
+++ Serial_Com.c
@@ -1,3 +1,4 @@
+    //adicionando bibliotecas
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
@@ -8,6 +9,7 @@
 #include "inc/font.h"
 #include "inc/ssd1306.h"
 
+//DEFINIÇÕES dos pinos e porta I2C
 #define LED_PIN_GREEN 11 //led verde
 #define LED_PIN_BLUE 12 //led azul
 #define LED_PIN_RED 13 //led vermelho
@@ -15,10 +17,10 @@
 #define LED_PIN 7 //pino de saída da matriz de led
 #define BOTAO_A 5 //pino saida botao a
 #define BOTAO_B 6 //pino saida botao b
-#define I2C_PORT i2c1
-#define I2C_SDA 14  
-#define I2C_SCL 15
-#define display_address 0x3C
+#define I2C_PORT i2c1 //porta I2C
+#define I2C_SDA 14 //pino SDA
+#define I2C_SCL 15 //pino SCL
+#define display_address 0x3C //endereço do display
 
 //VARIÁVEIS GLOBAIS
 int static volatile indice = 0; //variável para countrole do índice da matriz de led
@@ -28,9 +30,9 @@ uint valor_led; //variável para countrole do valor do led
 uint sm;  //variável para countrole do state machine
 PIO pio = pio0;  //variável para countrole da pio
 ssd1306_t ssd;  //variável para countrole do display
-bool GREEN_LED_OFF = false;
-bool BLUE_LED_OFF = false;
-bool RED_LED_OFF = false;
+bool GREEN_LED_OFF = false; //variável para countrole do led verde
+bool BLUE_LED_OFF = false;  //variável para countrole do led azul
+bool RED_LED_OFF = false;   //variável para countrole do led vermelho
 double r = 0.3, g = 0.3, b = 0.3;  // Variáveis para controlar a intensidade de cada cor
 int numero_anterior = -1; // Variável para armazenar o número anterior
 
@@ -146,7 +148,7 @@ double apagar_leds[25] ={   //Apagar LEDs da matriz
 
 double *numeros[11] = {numero0, numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8, numero9, apagar_leds};  // Vetor com os números
 
-
+// FUNÇÃO DE CALLBACK PARA OS BOTÕES
 void callback_button(uint gpio, uint32_t events) {
     uint time = to_ms_since_boot(get_absolute_time());
     if (time - actual_time > 250) { // Condição para evitar múltiplos pressionamentos (debounce)
@@ -172,6 +174,8 @@ void callback_button(uint gpio, uint32_t events) {
         }
 
     }
+
+    // Atualiza o display
     ssd1306_fill(&ssd, false); // Limpa o display
     // Imprime estado atual dos leds no display
     gpio_get(LED_PIN_GREEN) ? ssd1306_draw_string(&ssd, "LED Verde ON", 10, 10) :
@@ -184,13 +188,12 @@ void callback_button(uint gpio, uint32_t events) {
 int main(){
     bool frequenciaClock; // Variável para verificar se a frequência do clock foi configurada corretamente
     uint16_t i; // Variável para controlar o loop
-
     pio = pio0; // Seleciona a PIO 0
     uint32_t valor_led = 0; // Inicializa com preto (todos os LEDs apagados)
     char c; // Variável para armazenar o caractere recebido no monitor serial
-   
-
     frequenciaClock = set_sys_clock_khz(128000, false); // Configura a frequência do clock para 128 MHz
+
+    // Inicialização de todos os periféricos
     stdio_init_all(); 
     gpio_init(LED_PIN);
     gpio_init(LED_PIN_RED);
@@ -210,7 +213,7 @@ int main(){
     gpio_put(BLUE_LED_OFF, false);
     gpio_put(RED_LED_OFF, false);
     
-
+    // Inicialização e configuração do display
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); 
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
@@ -224,23 +227,27 @@ int main(){
     ssd1306_init(&ssd, 128, 64, false, display_address, I2C_PORT);
     ssd1306_config(&ssd);
 
+    
+    //vericação da frequência do clock
     printf("iniciando a transmissão PIO");
     if (frequenciaClock){
       printf("clock set to %ld\n", clock_get_hz(clk_sys));
     }else if(!frequenciaClock){
       printf("erro ao configurar a frequencia do clock");
     }
-      uint offset = pio_add_program(pio, &pio_matrix_program);
-      sm = pio_claim_unused_sm(pio, true);
-      pio_matrix_program_init(pio, sm, offset, LED_PIN);
+    
     
     // Inicializa a matriz de LED
-      desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b); // Apaga os LEDs ao iniciar o programa
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, LED_PIN);
+    desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b); // Apaga os LEDs ao iniciar o programa
 
-      // Configuração dos botões
-      gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &callback_button);  
-      gpio_set_irq_enabled_with_callback(BOTAO_B, GPIO_IRQ_EDGE_FALL, true, &callback_button);  
+    // Configuração dos botões
+    gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &callback_button);  
+    gpio_set_irq_enabled_with_callback(BOTAO_B, GPIO_IRQ_EDGE_FALL, true, &callback_button);  
 
+    // Loop principal
     while (true) {
         int c = getchar_timeout_us(0); // Verifica se há caractere disponível sem bloquear
 
@@ -248,18 +255,18 @@ int main(){
             printf("Caractere recebido: %c\n", (char)c);
             int numero = c - '0';
 
-            ssd1306_fill(&ssd, false);
+            ssd1306_fill(&ssd, false);  // Limpa o display
             ssd1306_draw_char(&ssd, (char)c, 20, 30); // Usando ssd1306_draw_char
-            ssd1306_send_data(&ssd);
-            
-            if (numero != numero_anterior) {
-                numero_anterior = numero;
+            ssd1306_send_data(&ssd);    // Atualiza o display
 
-                if (numero >= 0 && numero <= 9) {
-                    desenho_pio(numeros[numero], valor_led, pio, sm, r, g, b);
+            if (numero != numero_anterior) {    // Verifica se o número é diferente do anterior
+                numero_anterior = numero;    // Atualiza o número anterior
+
+                if (numero >= 0 && numero <= 9) {   // Verifica se o número é válido
+                    desenho_pio(numeros[numero], valor_led, pio, sm, r, g, b);  // Desenha o número na matriz de LED
                 } else {
-                    desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b);
-                    printf("Caractere inválido!\n");
+                    desenho_pio(apagar_leds, valor_led, pio, sm, r, g, b);  // Apaga os LEDs
+                    printf("Caractere inválido!\n");    // Imprime mensagem de erro
                 }
             }
         }
